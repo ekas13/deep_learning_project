5314e0fb727665d0d4afee034f666ba7a29b3d06
Noa Margeta <nmargeta@gmail.com>
2024-11-14 16:35:40 +0100

    Add batching to FID score calculation

--- src/evaluate.py
+++ src/evaluate.py
@@ -12,13 +12,20 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 ddpm = DDPM(device=device, network=score_network_0())
 ddpm.load("results/hpc_2000/testing")
 
-sampled_data = np.asarray([ddpm.sample().view(1, 28, 28).cpu().numpy() for _ in range(NUM_SAMPLES)])
+samples = []	
+
+for i in range(NUM_SAMPLES):
+    sample = ddpm.sample().view(1, 28, 28).cpu().numpy()
+    samples.append(sample)
+    print(f"Sample {i+1} generated")
+
+sampled_data = np.asarray(samples)
 np.save("results/hpc_2000/samples.npy", sampled_data)
 
 train_data_loader = data_loader("MNIST", 64, device)
 train_data = train_data_loader.x_train.view(-1, 1, 28, 28).cpu().numpy()[:NUM_SAMPLES, :]
 
-score = fid_score(sampled_data, train_data)
+score = fid_score(sampled_data, train_data, batch_size=64)
 
 with open("results/hpc_2000/fid_score.txt", "w") as f:
     f.write(f"FID score for {NUM_SAMPLES} samples: {score}")
--- src/metrics.py
+++ src/metrics.py
@@ -5,7 +5,7 @@ import tensorflow_hub as tfhub
 
 MNIST_MODULE = "https://tfhub.dev/tensorflow/tfgan/eval/mnist/logits/1"
 
-def fid_score(sampled_data, real_data):
+def fid_score(sampled_data: np.array, real_data: np.array, batch_size: int=64):
     # sampled_data and real_data have to be numpy arrays (samples, 1, 28, 28)
     sampled_data = tf.convert_to_tensor(sampled_data, dtype=tf.float32)
     real_data = tf.convert_to_tensor(real_data, dtype=tf.float32)
@@ -15,9 +15,20 @@ def fid_score(sampled_data, real_data):
     
     mnist_classifier_fn = tfhub.load(MNIST_MODULE)
     
+    sampled_features_list = []
+    real_features_list = []
+
     with tf.device('/CPU:0'):
-        sampled_features = mnist_classifier_fn(sampled_data)
-        real_features = mnist_classifier_fn(real_data) 
+        for i in range(0, sampled_data.shape[0], batch_size):
+            batch_sampled_data = sampled_data[i:i + batch_size]
+            batch_real_data = real_data[i:i + batch_size]
+            sampled_features = mnist_classifier_fn(batch_sampled_data)
+            real_features = mnist_classifier_fn(batch_real_data)
+            sampled_features_list.append(sampled_features)
+            real_features_list.append(real_features)
+
+    sampled_features = tf.concat(sampled_features_list, axis=0)
+    real_features = tf.concat(real_features_list, axis=0)
 
     sampled_features = sampled_features.numpy()
     real_features = real_features.numpy()
